7a613a21c2fe08c62ee2d0be5c85d1aeae949651
Jooseppi12 <urijozsef@europe.com>
2023-09-12 12:17:39 +0200

    Add AssemblyInfo and nuget package fix

--- build.fsx
+++ build.fsx
@@ -32,7 +32,18 @@ nuget Paket.Core prerelease //"
 #endif
 
 open WebSharper.Fake
+open Fake.DotNet
 
 LazyVersionFrom "WebSharper" |> WSTargets.Default
+|> fun args ->
+    { args with
+        Attributes =
+                [
+                    AssemblyInfo.Company "IntelliFactory"
+                    AssemblyInfo.Copyright "(c) IntelliFactory 2023"
+                    AssemblyInfo.Title "https://github.com/dotnet-websharper/leaflet"
+                    AssemblyInfo.Product "WebSharper Leaflet 1.9.4+"
+                ]
+    }
 |> MakeTargets
 |> RunTargets
